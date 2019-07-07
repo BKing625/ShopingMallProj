08e6811319bc31c4319566d53fd5aef4fad476ef
B.K.KIM <ddiddikk@naver.com>
2019-07-07 20:26:33 +0900

    create project

---
+++ src/main/java/com/cafe24/mallproject/MallprojectApplication.java
@@ -0,0 +1,13 @@
+package com.cafe24.mallproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallprojectApplication.class, args);
+    }
+
+}
